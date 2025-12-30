58859568473b6b287d6cdb38d7e58332662a3690
Taras Kovalyk <tarik2454@gmail.com>
2025-12-30 13:34:10 +0200

    refactor: update LeaderboardUser interface to include detailed user statistics and improve data structure

--- src/modules/leaderBoard/types.ts
+++ src/modules/leaderBoard/types.ts
@@ -1,6 +1,12 @@
-import type { User } from "../../../types";
+import type { User } from "../../../types/index";
 
-export interface LeaderboardUser extends User {
+export interface LeaderboardUser {
+  _id: string;
+  username: string;
+  gamesPlayed: number | null;
+  balance: number | null;
+  totalWagered: number | null;
+  totalWon: number | null;
   rank: number;
   winRate: string;
 }
