f88dbf994e519ceddfa234323237fb8c222c6c7d
Taras Kovalyk <tarik2454@gmail.com>
2025-12-30 19:57:07 +0200

    refactor: update authApi to dynamically set baseURL based on environment variables and improve SVG icon attributes for consistency

--- src/config/authApi.ts
+++ src/config/authApi.ts
@@ -16,10 +16,14 @@ class AuthApi {
   private api: AxiosInstance;
 
   constructor() {
+    const baseURL =
+      import.meta.env.VITE_API_BASE_URL ||
+      (import.meta.env.DEV
+        ? "http://localhost:3000/api"
+        : "https://backend-internship-js-hw-03-sky-rus.vercel.app/api");
+
     this.api = axios.create({
-      baseURL:
-        import.meta.env.VITE_API_BASE_URL ||
-        "https://backend-internship-js-hw-03-sky-rus.vercel.app/api",
+      baseURL,
     });
 
     this.setupInterceptors();
